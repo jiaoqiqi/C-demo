2ae8c1894408535e833676928b12d87295d50093
jiaoqiqi <1176932165@qq.com>
2017-04-14 15:51:55 +0800

    add prrintf longest list

--- 算法/longestList.c
+++ 算法/longestList.c
@@ -1,34 +1,52 @@
 #include <stdio.h>
-
-#define len(a) (sizeof(a) / sizeof(a[0])) //数组长度 
+#include <malloc.h>
+#define len(a) (sizeof(a) / sizeof(a[0]))  
 
 int lis(int arr[], int len)  
 {  
-    int longest[len];  
-    for (int i=0; i<len; i++)  
-        longest[i] = 1;  
-  
+    int longest[len];
+    int list[len]; 
+
+    for (int i=0 ; i<len; i++){
+        longest[i] = 1;
+    }  
+         
     for (int j=1; j<len; j++) {  
         for (int i=0; i<j; i++) {  
-            if (arr[j]>arr[i] && longest[j]<longest[i]+1){ //注意longest[j]<longest[i]+1这个条件，不能省略。  
-                longest[j] = longest[i] + 1; //计算以arr[j]结尾的序列的最长递增子序列长度  
+            if (arr[j]>arr[i] && longest[j]<longest[i]+1){   
+                longest[j] = longest[i] + 1;
             }  
         }  
-    }  
-  
+    }
+
     int max = 0;  
     for (int j=0; j<len; j++) { 
         printf("%d\n",longest[j]); 
         if (longest[j] > max) 
-            max = longest[j];  //从longest[j]中找出最大值  
-    }  
-    return max;  
+            max = longest[j];    
+    } 
+    printf("The longgest length is :%d\n",max );
+
+    //printf the longest list
+    int *temp;
+    temp=(int *)malloc(sizeof(int)*max);
+    int k=max,m=len-1;
+    for (;m>=0;m--)
+    {
+        if (longest[m]==k){
+            temp[k-1]=arr[m];
+            k--;
+        }
+    }
+    for(int i=0;i<max;i++)
+        printf("%d ",temp[i]);
+    return max;
+
 }  
   
 int main()  
 {  
-    int arr[] = {1, 4, 5, 6, 2, 3, 8}; //测试数组  
-    int ret = lis(arr, len(arr));  
-    printf("the longest list length: %d\n",ret );
+    int arr[] = {1, 4, 5, 6, 2, 3, 8};  
+    lis(arr, len(arr));
     return 0;  
 }  
